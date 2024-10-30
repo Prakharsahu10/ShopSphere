f765073260fab678ad5bceb925f68c2b9270b0a8
Prakharsahu10 <prakhars762@gmail.com>
2024-10-30 21:49:24 +0530

    first commit

---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import axios from "axios";
+
+function RegisterPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const response = await axios.post("/api/auth/register", {
+      name,
+      email,
+      password,
+    });
+    console.log(response.data);
+  };
+
+  return (
+    <form onSubmit={handleRegister}>
+      <h1>Register</h1>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Name"
+      />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
+}
+
+export default RegisterPage;
